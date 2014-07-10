8b8801448a3d649cdaa4400d34220b5d37e891c2
Rob Smith <kormoc@gmail.com>
2014-07-10 10:40:18 -0700

    Base packet struct

---
+++ MySQLProtocol/Packet.go
@@ -0,0 +1,10 @@
+package MySQLProtocol
+
+type Packet struct {
+    data []byte
+    offset uint
+}
+
+func (packet Packet) HasRemainingData() bool {
+    return uint(len(packet.data)) - packet.offset > 0
+}
