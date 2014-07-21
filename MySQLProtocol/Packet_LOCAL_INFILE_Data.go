516312147c4d16b1f4c9c5bfe6c8a118ec4cb364
Rob Smith <kormoc@gmail.com>
2014-07-20 20:07:32 -0700

    LOCAL_INFILE_Data

---
+++ MySQLProtocol/Packet_LOCAL_INFILE_Data.go
@@ -0,0 +1,28 @@
+package MySQLProtocol
+
+type Packet_LOCAL_INFILE_Data struct {
+	Packet
+    
+    data string
+}
+
+func (packet Packet_LOCAL_INFILE_Data) GetPacketSize(context Context) (size uint64) {
+    size += GetFixedLengthStringSize(packet.data)
+	return size
+}
+
+func (packet Packet_LOCAL_INFILE_Data) ToPacket(context Context) (data []byte) {
+	size := packet.GetPacketSize(context)
+
+	data = make([]byte, 0, size+4)
+	data = append(data, BuildFixedLengthInteger3(uint32(size))...)
+	data = append(data, BuildFixedLengthInteger1(packet.sequence_id)...)
+    data = append(data, BuildFixedLengthString(packet.data)...)
+	return data
+}
+
+func (packet *Packet_LOCAL_INFILE_Data) FromPacket(context Context, data Proto) {
+	data.GetFixedLengthInteger3()
+	packet.sequence_id = data.GetFixedLengthInteger1()
+    packet.data = data.GetFixedLengthString()
+}
