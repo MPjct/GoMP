a7dcd0682e51b157d318d66bf6441c984f04ff7b
Rob Smith <kormoc@gmail.com>
2016-06-27 20:06:08 -0700

    Formatting fixes

--- Plugins/Plugin.go
+++ Plugins/Plugin.go
@@ -3,67 +3,67 @@ package Plugin
 import "github.com/MPjct/GoMP/MySQLProtocol"
 
 type Plugin_interface interface {
-    init(context MySQLProtocol.Context)
-    read_handshake(context MySQLProtocol.Context)
-    send_handshake(context MySQLProtocol.Context)
-    read_auth(context MySQLProtocol.Context)
-    send_auth(context MySQLProtocol.Context)
-    read_auth_result(context MySQLProtocol.Context)
-    send_auth_result(context MySQLProtocol.Context)
-    read_query(context MySQLProtocol.Context)
-    send_query(context MySQLProtocol.Context)
-    read_query_result(context MySQLProtocol.Context)
-    send_query_result(context MySQLProtocol.Context)
-    cleanup(context MySQLProtocol.Context)
+	init(context MySQLProtocol.Context)
+	read_handshake(context MySQLProtocol.Context)
+	send_handshake(context MySQLProtocol.Context)
+	read_auth(context MySQLProtocol.Context)
+	send_auth(context MySQLProtocol.Context)
+	read_auth_result(context MySQLProtocol.Context)
+	send_auth_result(context MySQLProtocol.Context)
+	read_query(context MySQLProtocol.Context)
+	send_query(context MySQLProtocol.Context)
+	read_query_result(context MySQLProtocol.Context)
+	send_query_result(context MySQLProtocol.Context)
+	cleanup(context MySQLProtocol.Context)
 }
 
 type Plugin struct {
 }
 
 func (plugin *Plugin) init(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) read_handshake(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) send_handshake(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) read_auth(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) send_auth(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) read_auth_result(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) send_auth_result(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) read_query(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) send_query(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) read_query_result(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) send_query_result(context MySQLProtocol.Context) {
-    return
+	return
 }
 
 func (plugin *Plugin) cleanup(context MySQLProtocol.Context) {
-    return
+	return
 }
