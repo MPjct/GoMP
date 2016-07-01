8aa926c2ad8a03d4e4a952d53a08b1ec94363624
Rob Smith <kormoc@gmail.com>
2016-06-30 23:14:20 -0700

    Complete NULL Bitmap tests

--- MySQLProtocol/NULL_Bitmap_test.go
+++ MySQLProtocol/NULL_Bitmap_test.go
@@ -3,22 +3,29 @@ package MySQLProtocol
 import "testing"
 import "github.com/stretchr/testify/assert"
 
+var NULL_Bitmap_test_packets = []struct {
+	packet Proto
+}{
+	{packet: Proto{data: StringToPacket(`
+00 00
+`)}},
+}
+
 func Test_NULL_Bitmap(t *testing.T) {
-	var null_bitmap NULL_bitmap
+	for _, value := range NULL_Bitmap_test_packets {
 
-	null_bitmap.offset = 2
-	null_bitmap.num_fields = 9
-	null_bitmap.nulls = make([]byte, 2, 2)
+		null_bitmap := value.packet.GetNullBitmap(9, 2)
 
-	assert.Equal(t, null_bitmap.GetNullBitmapSize(), uint64(2), "")
+		assert.Equal(t, null_bitmap.GetNullBitmapSize(), uint64(2), "")
 
-	assert.Equal(t, null_bitmap.BuildNullBitmap(), []byte{0x0, 0x0}, "")
+		assert.Equal(t, null_bitmap.BuildNullBitmap(), []byte{0x0, 0x0}, "")
 
-	null_bitmap.SetField(8, true)
+		null_bitmap.SetField(8, true)
 
-	assert.Equal(t, null_bitmap.BuildNullBitmap(), []byte{0x0, 0x04}, "")
+		assert.Equal(t, null_bitmap.BuildNullBitmap(), []byte{0x0, 0x04}, "")
 
-	null_bitmap.SetField(8, false)
+		null_bitmap.SetField(8, false)
 
-	assert.Equal(t, null_bitmap.BuildNullBitmap(), []byte{0x0, 0x00}, "")
+		assert.Equal(t, null_bitmap.BuildNullBitmap(), []byte{0x0, 0x00}, "")
+	}
 }
